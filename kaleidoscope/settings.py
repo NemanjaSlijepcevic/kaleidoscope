9e2518a4ba69b1350c1a9040f5296c40fd456009
NemanjaSlijepcevic <126480733+NemanjaSlijepcevic@users.noreply.github.com>
2026-03-10 20:42:05 +0100

    Update settings.py

--- kaleidoscope/settings.py
+++ kaleidoscope/settings.py
@@ -147,9 +147,6 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
